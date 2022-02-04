f07b7e90487987a90baf81f0379265972929fc3b
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-02-05 01:56:31 +0900

    counter app.

--- counter_app/src/App.vue
+++ counter_app/src/App.vue
@@ -1,28 +1,8 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Sample msg1="Welcome to Your Vue.js App" msg2="hello training app" />
-  </div>
-</template>
-
-<script>
-import Sample from './components/Sample.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Sample
-  }
-}
+<script setup>
+import Counter from "./components/Counter.vue";
+import "./components/style.css";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <Counter />
+</template>
